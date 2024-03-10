6465e88ed75be76363d7b18121ce65416c107fb0
Kirubhakaran003 <149936232+Kirubhakaran003@users.noreply.github.com>
2024-03-11 01:57:20 +0530

    Delete Employee_Management/Employee_Management directory

--- Employee_Management/Employee_Management/src/main/java/Graded_Project/Employee_Management/model/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package Graded_Project.Employee_Management.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
